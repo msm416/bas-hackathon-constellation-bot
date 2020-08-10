158e277e02b4be85406aefdb8ac7bafe15c8cb5a
msm416-laptop <msm416@ic.ac.uk>
2020-08-11 01:49:15 +0300

    fix? 4

--- clock.py
+++ clock.py
@@ -3,10 +3,10 @@ from main import generateAndPost
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=5)
+@sched.scheduled_job('interval', minutes=2)
 def timed_job():
     generateAndPost()
-    print('This job is run every 5 minutes.')
+    print('This job is run every 2 minutes.')
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
